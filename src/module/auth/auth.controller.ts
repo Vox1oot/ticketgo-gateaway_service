09e824e619c876c31a2a2b35195639d6d05fccd8
Vitaliy <voxfoot@gmail.com>
2026-05-01 23:16:25 +0500

    feat: добавление документации API для отправки OTP и обновление DTO

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,9 +1,15 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { ApiOperation } from '@nestjs/swagger'
 
 import { SendOtpRequestDto } from './dto/send-otp.request.dto'
 
 @Controller('auth')
 export class AuthController {
+	@ApiOperation({
+		summary: 'Send OTP to a phone number',
+		description:
+			'Sends a one-time password (OTP) to the specified phone number for authentication purposes.'
+	})
 	@Post('otp/send')
 	@HttpCode(HttpStatus.OK)
 	public async sendOtp(@Body() dto: SendOtpRequestDto) {
--- src/module/auth/dto/send-otp.request.dto.ts
+++ src/module/auth/dto/send-otp.request.dto.ts
@@ -1,11 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEnum, IsString, Validate } from 'class-validator'
 import { IdentifierValidator } from 'src/shared/validators/identifier.lvalidator'
 
 export class SendOtpRequestDto {
+	@ApiProperty({
+		example: '+91234567890'
+	})
 	@IsString()
 	@Validate(IdentifierValidator)
 	public identifier!: string
 
+	@ApiProperty({
+		example: 'phone',
+		enum: ['email', 'phone']
+	})
 	@IsEnum(['email', 'phone'])
 	public type!: 'email' | 'phone'
 }
